67a8d7877a87f8b67ef8fe3364ee7613d0a463bc
Parth Patel <parth44@gmail.com>
2017-11-13 22:19:13 -0500

    update

--- resume.py
+++ resume.py
@@ -9,17 +9,7 @@ def home():
 
 @app.route('/courses')
 def courses():
-    courses = [
-        ['MISY 225', 'Introduction to Programming Business Applications',
-            'Learning to program in Java.'],
-        ['MISY 330', 'Database Design & Implementation',
-            'Learning data mining alogorithms and the SQL language'],
-        ['CISC 355', 'Computers, Ethics & Society',
-            'Analyzing the impact of computers on our society.'],
-        ['MISY 350', 'Web Application Development',
-            'Fundamentals of web development, including Python and Flask.']
-    ]
-    return render_template('courses.html', courses=courses)
+    return render_template('courses.html')
 
 
 @app.route('/about')
